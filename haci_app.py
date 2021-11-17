ff0c2102ed65ce0d13f71d3b2cd042ac981a0b8e
leon-acor-gmail <leon.acor@gmail.com>
2021-11-17 17:57:22 -0600

    Solución de errores y validaciones
    Se agregaron funciones de validación y se resolvieron errores de flujo y secuencia de los procesos

--- haci_app.py
+++ haci_app.py
@@ -1,40 +1,86 @@
-import re
+import re, json, hashlib, getpass, base64
 
 def fn_except_message(fn):
-    print('\n'+'Debes escribir solo 1 para "si" y 2 para "no"'+'---fn:'+fn)
+    print('\n'+'ATENCIÓN: Debes escribir solo 1 para "si" y 2 para "no"'+'---origen:'+fn)
 
-def fn_validate_usr(str_usr,int_is_user):
-    if not re.fullmatch(r"[^@]+@[^@]+\.[^@]+",str_usr):
-        print('\n'+'Debes de escribir una cuenta de correo valida')
-        fn_is_user(int_is_usr)
+def fn_validate_email(str_email):
+    if not re.fullmatch(r"[^@]+@[^@]+\.[^@]+",str_email):
+        return False
     else:
-        return str_usr
+        if len(str_email) > 0 and len(str_email) <= 40:
+            return True
+        else:
+            return False
+
+def fn_validate_pwd(str_pwd):
+    if len(str_pwd) >= 8 and len(str_pwd) <= 24:
+        return True
+    else:
+        return False
+
+def fn_validate_name(str_name):
+    if len(str_name) > 0 and len(str_name) <= 50:
+        return True
+    else:
+        return False
 
 def fn_print_header():
+    print('\n\n\n'+'INICIA PROGRAMA')
     print('\n\n\n'+'Hola estimado usuario, bienvenido a Hagamos Cine: Miles de empleos en todos los departamentos involucrados en la creación y distribución del cine en México y Latino-América.')
     print('\n'+'¿Qué deseas hacer?')
     fn_except_message('fn_print_header')
     str_is_user = input('\n'+'¿Ya tengo cuenta?'+'\n')
     return str_is_user.strip()
 
-def fn_is_user(str_is_user):
-    if str_is_user == 1:
-        str_usr = input('\n'+'Escribe tu correo'+'\n')
-        fn_validate_usr(str_usr,1)
-        str_pwd = input('\n'+'Escribe tu contraseña'+'\n')
-    elif str_is_user == 2:
-        str_new_user = input('\n'+'¿Deseas registrarte?'+'\n')
+def fn_is_user(int_is_user):
+    if int_is_user == 1:
+        print('\n\n\n'+'BIENVENIDO A HAGAMOS CINE - ACCEDER A LA PLATAFORMA')
+        str_email = input('\n'+'Escribe tu correo'+'\n')
+        if fn_validate_email(str_email):
+            str_pwd = input('\n'+'Escribe tu contraseña'+'\n')
+            #str_pwd = getpass.getpass()
+            if fn_validate_pwd(str_pwd):
+                obj_json = {'email':str_email,'pwd':hashlib.md5(str_pwd.encode()).hexdigest()}
+                str_json = json.dumps(obj_json)
+                str_base64 = base64.b64encode(str_json.encode('ascii'))
+                print(str_base64)
+                print('\n'+'Acceso concedido')
+            else:
+                print('\n'+'ERROR: Tu contraseña debe de tener más de 8 letras y/o simbolos y menos de 24 letrasy/o simbolos')
+                fn_is_user(int(fn_print_header()))
+        else:
+            print('\n'+'ERROR: Tu correo debe de tener menos de 40 letras y/o simbolos')
+            fn_is_user(int(fn_print_header()))
+    elif int_is_user == 2:
+        print('\n\n\n'+'BIENVENIDO A HAGAMOS CINE - CREAR CUENTA')
+        str_nam = input('\n'+'Escribe tu nombre'+'\n')
+        if fn_validate_name(str_nam):
+            str_email = input('\n'+'Escribe tu correo'+'\n')
+            if fn_validate_email(str_email):
+                str_pwd = input('\n'+'Escribe tu contraseña'+'\n')
+                #str_pwd = getpass.getpass()
+                if fn_validate_pwd(str_pwd):
+                    obj_json = {'name':str_nam,'email':str_email,'pwd':hashlib.md5(str_pwd.encode()).hexdigest()}
+                    str_json = json.dumps(obj_json)
+                    str_base64 = base64.b64encode(str_json.encode('ascii'))
+                    print(str_base64)
+                    print('\n'+'Registro exitoso')
+                else:
+                    print('\n'+'ERROR: Tu contraseña debe de tener más de 8 letras y/o simbolos y menos de 24 letrasy/o simbolos')
+                    fn_is_user(int(fn_print_header()))
+            else:
+                print('\n'+'ERROR: Tu correo debe de tener menos de 40 letras y/o simbolos')
+                fn_is_user(int(fn_print_header()))
+        else:
+            print('\n'+'ERROR: Tu nombre debe de tener menos de 50 letras y/o simbolos')
+            fn_is_user(int(fn_print_header()))
+
     else:
-        print('leo')
         fn_except_message('fn_is_user')
-        #print('\n'+'Debes escribir solo 1 para "si" y 2 para "no"')
 
 if __name__ == '__main__':
-    str_is_user = fn_print_header()
     try:
-        int_is_user = int(str_is_user)
-        fn_is_user(int_is_user)
+        fn_is_user(int(fn_print_header()))
+        print('\n\n\n'+'TERMINA PROGRAMA')
     except:
         fn_except_message('fn_main')
-        #print('\n'+'Debes escribir solo 1 para "si" y 2 para "no"')
-        fn_print_header()
